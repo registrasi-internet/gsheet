dbccb44f2f5a438212ceefdaf2a1ebda30a5e568
registrasi-internet <kazien88@gmail.com>
2026-01-27 11:31:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzPdl7u6HcFLR4ZNqsE_xYgnNZ2isb0N5Xwdapkumcc2T2EENKZkh_co-wCD_6vlfzcEQ/exec"; // Ganti dengan URL Anda
-const GEMINI_API_KEY = "AIzaSyDpBo5deWTKkkgziQCL2Xr7HOC3Ghyos6c";
+const GEMINI_API_KEY = "AIzaSyAkN6IbmJxPekfcNwPRffkO5FXatgAf2Os";
 
 export { SCRIPT_URL, GEMINI_API_KEY };
